0a4f6528e69bb1f11a70878b3ae833a9c55def57
JCly-rikiu <jk123456789jp@gmail.com>
2016-10-26 14:25:49 +0800

    add sequence to Packet

--- src/ntu/csie/ping/Packet.java
+++ src/ntu/csie/ping/Packet.java
@@ -5,12 +5,18 @@ import java.io.Serializable;
 public class Packet implements Serializable {
 
   private long sendTime;
+  private int seq;
 
-  public Packet(long sendTime) {
+  public Packet(long sendTime, int seq) {
     this.sendTime = sendTime;
+    this.seq = seq;
   }
 
   public long getRTT(long receiveTime) {
     return receiveTime - sendTime;
+  }
+
+  public int getSeq() {
+    return seq;
   }
 }
